802aa36d4b4a6a1e9d523e0207fc40c92f527ed2
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2026-05-15 18:07:34 -0300

    fix: types AcaiRouterPageError & AcaiRouterPageSplash

--- src/runtime/router.ts
+++ src/runtime/router.ts
@@ -7,8 +7,8 @@ export type AcaiRouterPage<T = {}> = (props: T, std: GlyStd) =>
   | Promise<JSX.Element>
   | AsyncGenerator<JSX.Element, JSX.Element | void, unknown>;
 
-export type AcaiRouterPageError = (props: {getMessage: (this: void) => string}, std: GlyStd) => JSX.Element
-export type AcaiRouterPageSplash = (props: {}, std: GlyStd) => JSX.Element
+export type AcaiRouterPageError = (this: void, props: {getMessage: (this: void) => string}, std: GlyStd) => JSX.Element
+export type AcaiRouterPageSplash = (this: void, props: {}, std: GlyStd) => JSX.Element
 export type AcaiRouterEndpoints<P = any> = Record<AcaiRouterString, AcaiRouterPage<P>>;
 
 type Primitive = string | number | boolean | undefined;
